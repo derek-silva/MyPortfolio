48d47e3564e4bb3d7d0d8aa80103e1e2b60ac934
Derek Silva <dereksilva.tech@gmail.com>
2019-01-29 12:51:26 -0600

    Refactored strong params for portfolio items, added Topic selection for new portfolio item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,5 @@
 class PortfoliosController < ApplicationController
-	# before_action :set_blog [:show, :edit, :update, :destroy]
+	before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@portfolio_items = Portfolio.all
@@ -15,11 +15,10 @@ class PortfoliosController < ApplicationController
 	end
 
 	def show
-		@portfolio_item = Portfolio.find(params[:id])
 	end
 
 	def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -31,14 +30,12 @@ class PortfoliosController < ApplicationController
 	end
 
 	def edit
-		@portfolio_item = Portfolio.find(params[:id])
 	end
 	
 	def update
-		@portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
       else
         format.html { render :edit }
@@ -47,13 +44,24 @@ class PortfoliosController < ApplicationController
 	end
 	
 	def destroy
-		@portfolio_item = Portfolio.find(params[:id])
-
 		@portfolio_item.destroy
 		
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully deleted.' }
     end
-  end
+	end
+	
+	private
+	# Use callbacks to share common setup or constraints between actions.
+	def set_portfolio
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def portfolio_params
+		params.require(:portfolio).permit(:title, 
+																			:subtitle, 
+																			:body, technologies_attributes: [:name])
+	end
 
 end
